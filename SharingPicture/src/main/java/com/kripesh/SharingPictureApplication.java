f56243a593585b5e6439ce47d008278cee300d19
kbista <kbista@intellimec.com>
2017-03-07 17:35:33 +0545

    Initial Commit of my Project

---
+++ SharingPicture/src/main/java/com/kripesh/SharingPictureApplication.java
@@ -0,0 +1,12 @@
+package com.kripesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingPictureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingPictureApplication.class, args);
+	}
+}
