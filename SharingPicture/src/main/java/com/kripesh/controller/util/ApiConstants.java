0433de0e1bcc3bd780feaebca05569bcbfe8e133
kbista <kbista@intellimec.com>
2017-03-08 21:44:48 +0545

    Configured hibernate for data persistence

--- SharingPicture/src/main/java/com/kripesh/controller/util/ApiConstants.java
+++ SharingPicture/src/main/java/com/kripesh/controller/util/ApiConstants.java
@@ -1,9 +1,12 @@
 package com.kripesh.controller.util;
-
+/**
+ * 
+ * @author kripesh
+ *
+ */
 public class ApiConstants {
 
-
-    public static final String API_VER = "/api/v1";
-	public static final String USER_PATH ="/user";
-	public static final String USER_REGISTER_PATH ="/register";
+	public static final String API_VER = "/api/v1";
+	public static final String USER_PATH = "/user";
+	public static final String USER_REGISTER_PATH = "/register";
 }
