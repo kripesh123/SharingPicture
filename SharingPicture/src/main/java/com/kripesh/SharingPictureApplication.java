c432f445823bc78c15b404e85fb25003e839a24f
kbista <kbista@intellimec.com>
2017-03-09 12:34:39 +0545

    Added java jwt filter and corrected version for dependency

--- SharingPicture/src/main/java/com/kripesh/SharingPictureApplication.java
+++ SharingPicture/src/main/java/com/kripesh/SharingPictureApplication.java
@@ -2,10 +2,23 @@ package com.kripesh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.kripesh.config.JwtFilter;
 
 @SpringBootApplication
 public class SharingPictureApplication {
 
+	@Bean
+	public FilterRegistrationBean jwtFilter(){
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/rest/*");
+		return registrationBean;
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SharingPictureApplication.class, args);
 	}
