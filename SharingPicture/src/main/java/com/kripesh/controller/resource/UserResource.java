665a0898ad0936a368121adb145522dd8fb63a8b
kbista <kbista@intellimec.com>
2017-03-10 16:37:57 +0545

    Done with the my album and increase the multipart configured file size

--- SharingPicture/src/main/java/com/kripesh/controller/resource/UserResource.java
+++ SharingPicture/src/main/java/com/kripesh/controller/resource/UserResource.java
@@ -3,7 +3,9 @@ package com.kripesh.controller.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kripesh.model.User;
@@ -19,4 +21,15 @@ public class UserResource {
 	public List<User> findAllUsers(){
 		return userService.findAll();
 	}
+	
+	@RequestMapping(value = "/user/userName", method = RequestMethod.POST)
+	public User findByUserName(@RequestBody String userName) {
+		return userService.findByUserName(userName);
+	}
+	
+	@RequestMapping(value = "/user/update", method = RequestMethod.POST)
+    public User updateUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+	
 }
