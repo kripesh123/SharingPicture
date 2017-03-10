1299f841049356f4d689fa5d17656f7cd280b382
kbista <kbista@intellimec.com>
2017-03-10 11:44:45 +0545

    Added Repository for photo dao

--- SharingPicture/src/main/java/com/kripesh/dao/PhotoDao.java
+++ SharingPicture/src/main/java/com/kripesh/dao/PhotoDao.java
@@ -2,13 +2,14 @@ package com.kripesh.dao;
 
 import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.kripesh.model.Photo;
 import com.kripesh.model.User;
 
 @Repository
-public interface PhotoDao{
+public interface PhotoDao extends CrudRepository<Photo, Long>{
 
 	List<Photo> findAll();
 	
